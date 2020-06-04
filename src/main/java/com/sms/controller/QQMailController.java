2989f9e6fff645c81d076be0030ad8f67af1b03e
lhw <1914526816@qq.com>
2020-06-04 10:31:40 +0800

    新增QQ邮箱发送邮件

--- src/main/java/com/sms/controller/QQMailController.java
+++ src/main/java/com/sms/controller/QQMailController.java
@@ -3,7 +3,6 @@ package com.sms.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.sms.service.QQMailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
